800948b57aff3370f5802b87a2fce05938889619
Justin Neigh <zimiyi00@gmail.com>
2022-04-21 23:02:30 -0400

    Loaded in the csv file, rearranged the data and split it into testing and training data

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy
+import numpy as np
+import csv
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Reads in the csv file and stores it into a list
+with open("kr-vs-kp_csv.csv") as file_name:
+    file_read = csv.reader(file_name)
+    array = list(file_read)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#converts the list into a numpy array
+data = numpy.array(array)
+
+#loops through each row to change the values
+for x in range(len(data)):
+    for y in range(len(data[x])):
+        element = data[x][y]
+        if element == 'f' or element == 'l': #if the current element is a f or a l then it changes it to a zero
+            data[x][y] = 0
+        if(element == 't' or element == 'g'): #if the current element is a t or a g then it changes it to a one
+            data[x][y] = 1
+
+
+splitIndex = int(0.75*len(data)) #gets the index to split the data 75/25
+trainingData = data[:splitIndex] #stores 75 percent of the data into training
+testingData = data[splitIndex:]  #stores remaining 25 percent of data into testing
+
+print(trainingData)
+print("This is new line")
+print(testingData)
+
+
+
